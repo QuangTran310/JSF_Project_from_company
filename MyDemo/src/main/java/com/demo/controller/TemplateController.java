0f676e63fce6c84dd682eecc73376aa053820fc2
QuangQuang <tranminhquang311097@gmail.com>
2022-04-13 17:08:35 +0700

    updated: 13-04-2022
    tìm hiểu tại sao Conversation lại begin 1 lần cuối sau khi đã end.

--- MyDemo/src/main/java/com/demo/controller/TemplateController.java
+++ MyDemo/src/main/java/com/demo/controller/TemplateController.java
@@ -2,11 +2,16 @@ package com.demo.controller;
 
 import java.io.Serializable;
 
-import javax.enterprise.context.SessionScoped;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 @Named(value = "templateControllerUIBean")
-@SessionScoped
+@ConversationScoped
 public class TemplateController implements Serializable {
 
 	/**
@@ -14,16 +19,49 @@ public class TemplateController implements Serializable {
 	 */
 	private static final long serialVersionUID = 1718630665473723417L;
 
+	@Inject
+	Conversation conversation;
+
 	private String templateCode = "home";
+
+	@PostConstruct
+	public void initBean() {
+		System.out.println("Start initiate bean 'templateControllerUIBean'.");
+	}
+
+	@PreDestroy
+	public void destroyBean() {
+		System.out.println("Destroy bean 'templateControllerUIBean'.");
+	}
+
+	public void initConversation() {
+		if (!FacesContext.getCurrentInstance().isPostback() && conversation.isTransient()) {
+			conversation.begin();
+		}
+		System.out.println("Conversation has been beginned.");
+	}
+
+	public void endConversation() {
+		if (!conversation.isTransient()) {
+			conversation.end();
+		}
+		System.out.println("Conversation has been finished.");
+	}
+
+	public Conversation getConversation() {
+		return conversation;
+	}
+
+	public void setConversation(Conversation conversation) {
+		this.conversation = conversation;
+	}
 
 	public String getTemplateCode() {
 		return templateCode;
 	}
 
 	public void setTemplateCode(String templateCode) {
-		System.out.println("On processing to change template code ...");
 		this.templateCode = templateCode;
-		System.out.println("Template code has been changed to '" + this.templateCode + "'");
 	}
 
 }
